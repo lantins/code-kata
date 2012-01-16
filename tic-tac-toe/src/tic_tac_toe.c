8cfdb8f784a1c81731585ef4561313a74e0c0c2e
Luke Antins <luke@lividpenguin.com>
2012-01-16 01:45:12 +0000

    Smallest amount of code to make the test pass.

--- tic-tac-toe/src/tic_tac_toe.c
+++ tic-tac-toe/src/tic_tac_toe.c
@@ -1,6 +1,9 @@
 #include "tic_tac_toe.h"
 
 int lp_ttt_play(int moves[]) {
+    if(moves[1]) {
+        return kLP_TTT_STATE_TURN_X;
+    }
     if(moves[0]) {
         return kLP_TTT_STATE_TURN_O;
     }
